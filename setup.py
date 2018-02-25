1dc15b89f382637be73190194bf68ceb57d271e8
justin <justin.barry.e@knights.ucf.edu>
2018-02-25 01:39:40 -0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+from setuptools import setup, find_packages
+
+setup(
+    name='image',
+    version='1.0.0',
+    description='Following sentdex youtube example for image classification with dnn',
+    url='',
+    author='jb',
+    email='justin.barry.e@knights.ucf.edu',
+    packages=find_packages()
+)
