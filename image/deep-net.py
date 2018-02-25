5ee986f51a965a5b3bad64bc5fb2394a2ef3e539
justin <justin.barry.e@knights.ucf.edu>
2018-02-25 02:16:40 -0500

    creating training function

--- image/deep-net.py
+++ image/deep-net.py
@@ -43,3 +43,33 @@ def neural_network_model(data):
     output = tf.matmul(l3, output_layer['weights']) + output_layer['biases']
 
     return output
+
+
+def train_nn(x):
+    prediction = neural_network_model(x)
+    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(prediction, y))
+
+    # learning_rate = (default) 0.001
+    optimizer = tf.train.AdamOptimizer().minimize(cost)
+
+    # cycles feed forward + back prop
+    hm_epochs = 10
+
+    with tf.Session() as sess:
+        sess.run(tf.initialize_all_variables())
+
+        for epoch in hm_epochs:
+            epoch_loss = 0
+            for _ in range(int(mnist.train.num_examples / batch_size)):
+                # in reality we have to write a function that would do this for uss, but
+                # tensorflow does this for us
+                x, y = mnist.train.next_batch(batch_size)
+                _, c = sess.run([optimizer, cost], feed_dict={x: x, y: y})
+                epoch_loss += c
+            print('Epoch', epoch, 'completed out of', hm_epochs, 'loss: ', epoch_loss)
+
+        correct = tf.equal(tf.argmax(prediction, 1), tf.argmax(y, 1))
+
+        accuracy = tf.reduce_mean(tf.cast(correct, 'float'))
+        print('Accuracy: {0}'.format(
+            accuracy.eval({x: mnist.test.images, y: mnist.test.labels})))
