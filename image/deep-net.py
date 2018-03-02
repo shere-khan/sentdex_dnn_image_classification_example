8e0ac5ee88be5e5f1b6f0d5c160fd4d01d257093
justin <justin.barry.e@knights.ucf.edu>
2018-03-01 22:34:18 -0500

    code works

--- image/deep-net.py
+++ image/deep-net.py
@@ -16,28 +16,28 @@ y = tf.placeholder('float')
 
 def neural_network_model(data):
     hidden_1_layer = {'weights': tf.Variable(tf.random_normal([784, n_nodes_hl1])),
-                      'biases': tf.Variable(tf.random_normal(n_nodes_hl1))}
+                      'biases': tf.Variable(tf.random_normal([n_nodes_hl1]))}
 
     hidden_2_layer = {
         'weights': tf.Variable(tf.random_normal([n_nodes_hl1, n_nodes_hl2])),
-        'biases': tf.Variable(tf.random_normal(n_nodes_hl2))}
+        'biases': tf.Variable(tf.random_normal([n_nodes_hl2]))}
 
     hidden_3_layer = {
         'weights': tf.Variable(tf.random_normal([n_nodes_hl2, n_nodes_hl3])),
-        'biases': tf.Variable(tf.random_normal(n_nodes_hl3))}
+        'biases': tf.Variable(tf.random_normal([n_nodes_hl3]))}
 
     output_layer = {'weights': tf.Variable(tf.random_normal([n_nodes_hl3, n_classes])),
                     'biases': tf.Variable(tf.random_normal([n_classes]))}
 
     # (input_data * weights) + biases
 
-    l1 = tf.add(tf.matmul(data, hidden_1_layer['weights']) + hidden_1_layer['biases'])
+    l1 = tf.add(tf.matmul(data, hidden_1_layer['weights']), hidden_1_layer['biases'])
     l1 = tf.nn.relu(l1)
 
-    l2 = tf.add(tf.matmul(l1, hidden_2_layer['weights']) + hidden_2_layer['biases'])
+    l2 = tf.add(tf.matmul(l1, hidden_2_layer['weights']), hidden_2_layer['biases'])
     l2 = tf.nn.relu(l2)
 
-    l3 = tf.add(tf.matmul(l2, hidden_3_layer['weights']) + hidden_3_layer['biases'])
+    l3 = tf.add(tf.matmul(l2, hidden_3_layer['weights']), hidden_3_layer['biases'])
     l3 = tf.nn.relu(l3)
 
     output = tf.matmul(l3, output_layer['weights']) + output_layer['biases']
@@ -47,7 +47,8 @@ def neural_network_model(data):
 
 def train_nn(x):
     prediction = neural_network_model(x)
-    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(prediction, y))
+    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=prediction,
+                                                                  labels=y))
 
     # learning_rate = (default) 0.001
     optimizer = tf.train.AdamOptimizer().minimize(cost)
@@ -58,13 +59,14 @@ def train_nn(x):
     with tf.Session() as sess:
         sess.run(tf.initialize_all_variables())
 
-        for epoch in hm_epochs:
+        for epoch in range(hm_epochs):
             epoch_loss = 0
             for _ in range(int(mnist.train.num_examples / batch_size)):
                 # in reality we have to write a function that would do this for uss, but
                 # tensorflow does this for us
-                x, y = mnist.train.next_batch(batch_size)
-                _, c = sess.run([optimizer, cost], feed_dict={x: x, y: y})
+                epoch_x, epoch_y = mnist.train.next_batch(batch_size)
+                _, c = sess.run([optimizer, cost],
+                                feed_dict={x: epoch_x, y: epoch_y})
                 epoch_loss += c
             print('Epoch', epoch, 'completed out of', hm_epochs, 'loss: ', epoch_loss)
 
@@ -73,3 +75,6 @@ def train_nn(x):
         accuracy = tf.reduce_mean(tf.cast(correct, 'float'))
         print('Accuracy: {0}'.format(
             accuracy.eval({x: mnist.test.images, y: mnist.test.labels})))
+
+
+train_nn(x)
